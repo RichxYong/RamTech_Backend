574e35bc2f9edb17ba5b6cf839a8460652f89c3b
Your Name <your.email@example.com>
2026-01-08 08:21:47 +0000

    Connected Railway MySQL

--- database/database.js
+++ database/database.js
@@ -6,16 +6,17 @@ const dotenv = require("dotenv").config();
 const mysql = require('mysql2');
 const Sequelize =require("sequelize");
 
-const db =new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host:process.env.DB_HOST,
-        dialect:"mysql",
-    }
-
-  );
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: 'mysql',
+    logging: false
+  }
+);
 
 db.authenticate()
   .then(async ()=>{
